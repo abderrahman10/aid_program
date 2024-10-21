6a3ee3db78453ddc691114f57c725e02291732c9
abderrahman rouas <prof.abder@gmail.com>
2024-10-21 01:09:06 +0100

    initial commit

---
+++ src/main/java/com/aide_budget/aide_budget/AideBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.aide_budget.aide_budget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AideBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AideBudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aide_budget/aide_budget/AideBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aide_budget.aide_budget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AideBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
